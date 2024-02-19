1178cc9458b0236eeba0c133d2c3c563d23430e5
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-19 18:00:52 +1100

    custom tailwind colours and widths added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,31 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      width: {
+        "128": "50rem",
+        "144": "56rem",
+        "160": "64rem",
+        "176": "72rem",
+        "192": "80rem",
+        "208": "88rem",
+      },
+      screens: {
+        "3xl": "1800px",
+      },
+      colors: {
+        primary: "#acf7c1",
+        secondary: " #ccfff4",
+        brand: "#7df2cd",
+        brandSecondary: "#c470e7",
+        dark: "#011627",
+        light: "#f7ece1",
+        altPrimary: "#0A5B5A", //#0E7C7B",
+        altSecondary: "#86bbbd",
+      },
+      transitionProperty: {
+        "max-height": "max-height",
+        visibility: "visibility",
+      },
     },
   },
   plugins: [],
