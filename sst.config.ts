e848c0c9d14f872837ae8dcd69f6f2ee246be099
Jack-Desktop <jackpinkerton@live.com.au>
2024-02-23 20:45:00 +1100

    different env files used for prod and dev deploys

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
+import dotenv from "dotenv";
 
 export default {
   config(_input) {
@@ -10,7 +11,15 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      // get stage
+      const { stage } = stack;
+      // get path to .env file for given stage
+      const path = envPathMap.get(stage);
+      // use dotenv to get the env file
+      const { parsed: environment } = dotenv.config({ path });
+      const site = new NextjsSite(stack, "site", {
+        environment,
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
@@ -18,3 +27,8 @@ export default {
     });
   },
 } satisfies SSTConfig;
+
+const envPathMap = new Map();
+envPathMap.set("dev", ".env.dev");
+envPathMap.set("staging", ".env.staging");
+envPathMap.set("prod", ".env.prod");
