2b6bf67125afd191ad443f8ea69f0e43b3b4e804
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-03 08:17:39 +1100

    changed custom .env flow, moving this to seed deployments

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
-import dotenv from "dotenv";
 
 export default {
   config(_input) {
@@ -11,15 +10,7 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      // get stage
-      const { stage } = stack;
-      // get path to .env file for given stage
-      const path = envPathMap.get(stage);
-      // use dotenv to get the env file
-      const { parsed: environment } = dotenv.config({ path });
-      const site = new NextjsSite(stack, "site", {
-        environment,
-      });
+      const site = new NextjsSite(stack, "site");
 
       stack.addOutputs({
         SiteUrl: site.url,
@@ -27,8 +18,3 @@ export default {
     });
   },
 } satisfies SSTConfig;
-
-const envPathMap = new Map();
-envPathMap.set("dev", ".env.dev");
-envPathMap.set("staging", ".env.staging");
-envPathMap.set("prod", ".env.prod");
