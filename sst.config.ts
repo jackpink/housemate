69791788ff3a6aeb7271b91fb72c83b7fa07917e
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-04 13:19:30 +1100

    add env to sst build

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { Config, NextjsSite } from "sst/constructs";
+import { env } from "~/env";
 
 export default {
   config(_input) {
@@ -10,7 +11,15 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const CLERK_SECRET_KEY = new Config.Secret(stack, "CLERK_SECRET_KEY");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          CLERK_SECRET_KEY: env.CLERK_SECRET_KEY,
+          DATABASE_URL: env.DATABASE_URL,
+          UPSTASH_REDIS_REST_URL: env.UPSTASH_REDIS_REST_URL,
+          UPSTASH_REDIS_REST_TOKEN: env.UPSTASH_REDIS_REST_TOKEN,
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
