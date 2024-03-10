1e9d1a6373845e343313e233a6cc1b9038667c4e
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-11 09:33:14 +1100

    api router posts removed

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,7 +7,6 @@ import {
   publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -17,26 +16,4 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}, you are ${ctx.user?.firstName}!`,
       };
     }),
-
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // Check rate limit
-      const { success } = await rateLimiter.limit(ctx.auth.userId);
-      if (!success) {
-        throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-      }
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      await ctx.db.insert(posts).values({
-        name: input.name,
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
-  }),
 });
