a580a4e94b70887afdb8ad2645dbb1443936f574
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-11 16:46:25 +1100

    added domain settings

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { Config, NextjsSite } from "sst/constructs";
+import { NextjsSite } from "sst/constructs";
 import { env } from "~/env";
 
 export default {
@@ -11,13 +11,19 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const CLERK_SECRET_KEY = new Config.Secret(stack, "CLERK_SECRET_KEY");
       const site = new NextjsSite(stack, "site", {
         environment: {
           CLERK_SECRET_KEY: env.CLERK_SECRET_KEY,
           DATABASE_URL: env.DATABASE_URL,
           UPSTASH_REDIS_REST_URL: env.UPSTASH_REDIS_REST_URL,
           UPSTASH_REDIS_REST_TOKEN: env.UPSTASH_REDIS_REST_TOKEN,
+        },
+        customDomain: {
+          domainName:
+            stack.stage === "prod"
+              ? "housemate.dev"
+              : `${stack.stage}.housemate.dev`,
+          domainAlias: stack.stage === "prod" ? "www.housemate.dev" : undefined,
         },
       });
 
