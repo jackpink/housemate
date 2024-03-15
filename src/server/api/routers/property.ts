07c32bc2b34c5e3d39db25df22da1dabeccc4c71
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-15 14:14:50 +1100

    get valid address mutation added

--- src/server/api/routers/property.ts
+++ src/server/api/routers/property.ts
@@ -3,8 +3,89 @@ import { z } from "zod";
 import { rateLimiter } from "~/server/api/ratelimiter";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { property } from "~/server/db/schema";
+import axios from "axios";
+import { env } from "~/env";
+
+function isKeyOfObject<T extends object>(
+  key: string | number | symbol,
+  object: T,
+): key is keyof T {
+  return key in object;
+}
+
+type IComponentName = {
+  text: string;
+};
+
+type IAddressComponent = {
+  componentType: string;
+  componentName: IComponentName;
+};
+
+interface IGoogleApiData {
+  result: IGoogleApiResult;
+}
+
+interface IGoogleApiResult {
+  address: IGoogleApiAddress;
+}
+
+interface IGoogleApiAddress {
+  addressComponents: IAddressComponent[];
+}
+
+const googleAPINameMappings = {
+  subpremise: "apartment",
+  street_number: "streetNumber",
+  route: "street",
+  country: "country",
+  locality: "suburb",
+  administrative_area_level_1: "state",
+  postal_code: "postcode",
+};
 
 export const propertyRouter = createTRPCRouter({
+  getValidAddress: protectedProcedure
+    .input(z.object({ addressSearchString: z.string() }))
+    .mutation(async ({ input }) => {
+      const client = axios.create();
+      const googleAddressValidationEndpoint =
+        "https://addressvalidation.googleapis.com/v1:validateAddress?key=" +
+        env.GOOGLE_MAPS_API_KEY;
+      const requestBody = {
+        address: {
+          regionCode: "AU",
+          addressLines: [input.addressSearchString],
+        },
+      };
+      const response = await client.post(
+        googleAddressValidationEndpoint,
+        requestBody,
+      );
+      const AddressObj: IAddress = {
+        apartment: null,
+        streetNumber: "",
+        street: "",
+        suburb: "",
+        postcode: "",
+        state: "",
+        country: "",
+      };
+
+      const returnData = response.data as IGoogleApiData;
+      const addressComponents = returnData.result.address.addressComponents;
+
+      for (const addressComponent of addressComponents) {
+        const componentType = addressComponent.componentType;
+        // check that the componentType is corect
+        if (isKeyOfObject(componentType, googleAPINameMappings)) {
+          const field = googleAPINameMappings[componentType];
+          const value = addressComponent.componentName.text;
+          if (isKeyOfObject(field, AddressObj)) AddressObj[field] = value;
+        }
+      }
+      return AddressObj;
+    }),
   create: protectedProcedure
     .input(
       z.object({
