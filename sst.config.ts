b3da74a0d5f5a288d7e8f9367af1915641ecb3bb
Jack-Desktop <jackpinkerton@live.com.au>
2024-03-18 20:29:23 +1100

    added file upload bucket

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { Bucket, NextjsSite } from "sst/constructs";
 import { env } from "~/env";
 
 export default {
@@ -11,7 +11,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const propertyCoverImageBucket = new Bucket(
+        stack,
+        "PropertyCoverImageBucket",
+      );
       const site = new NextjsSite(stack, "site", {
+        bind: [propertyCoverImageBucket],
         environment: {
           CLERK_SECRET_KEY: env.CLERK_SECRET_KEY,
           DATABASE_URL: env.DATABASE_URL,
@@ -27,7 +32,10 @@ export default {
           NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
             env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
           GOOGLE_MAPS_API_KEY: env.GOOGLE_MAPS_API_KEY,
+          NEXT_PUBLIC_PROPERTY_COVER_IMAGE_BUCKET_NAME:
+            propertyCoverImageBucket.bucketName,
         },
+
         /*customDomain:
           stack.stage === "prod"
             ? {
