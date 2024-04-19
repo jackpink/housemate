f21c3030205c15219c686ec527190ebb75f180e5
Jack-Desktop <jackpinkerton@live.com.au>
2024-04-19 16:39:40 +1000

    deploy stacks added

---
+++ stacks/Default.ts
@@ -0,0 +1,30 @@
+import { StackContext, NextjsSite } from "sst/constructs";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { env } from "./env";
+
+export function Default({ stack }: StackContext) {
+  const site = new NextjsSite(stack, "site", {
+    path: "packages/web-2",
+  });
+  stack.addOutputs({
+    SiteUrl: site.url,
+  });
+}
+
+export function TradeSite({ stack }: StackContext) {
+      const site = new NextjsSite(stack, "site", {
+      
+        environment: {
+        },
+        customDomain: {
+          domainName: "housemate.dev",
+          isExternalDomain: true,
+          cdk: {
+            certificate: Certificate.fromCertificateArn(
+              stack,
+              "Certificate",
+              env.AWS_SSL_CERTIFICATE_ARN,
+            ),
+          },
+          domainAlias: "www.housemate.dev",
+        },
