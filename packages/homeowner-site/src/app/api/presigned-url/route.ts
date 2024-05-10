b230c00d024d3dbf09210684802d8d7d8a3d81fc
Jack-Desktop <jackpinkerton@live.com.au>
2024-05-10 16:15:31 +1000

    presigned takes bucket name as input

--- packages/homeowner-site/src/app/api/presigned-url/route.ts
+++ packages/homeowner-site/src/app/api/presigned-url/route.ts
@@ -1,7 +1,6 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextRequest } from "next/server";
-import { Bucket } from "sst/node/bucket";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
