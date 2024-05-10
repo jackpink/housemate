8cc5997b5fdba5c6a71bf5bbb4f7bf962a912d84
Jack-Desktop <jackpinkerton@live.com.au>
2024-05-10 10:21:41 +1000

    image uploader handles multiple files and errors much better

--- packages/ui/Molecules/ImageUploader.tsx
+++ packages/ui/Molecules/ImageUploader.tsx
@@ -1,16 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useId } from "react";
 import axios from "axios";
 import clsx from "clsx";
 import { useState } from "react";
 import { CTAButton } from "../Atoms/Button";
 import { ErrorMessage, Text } from "../Atoms/Text";
+import {
+  CancelIcon,
+  ConfirmIcon,
+  PdfFileIcon,
+  RetryIcon,
+  UploadIcon,
+  WarningIcon,
+} from "../Atoms/Icons";
+
+type UploadStatus =
+  | "fileSizeError"
+  | "fileTypeError"
+  | "preview"
+  | "uploading"
+  | "success"
+  | "error";
 
 type Upload = {
   file: File;
   progress: number;
-  uploading: boolean;
+  status: UploadStatus;
 };
 
 export default function ImageUploader({
@@ -25,23 +41,90 @@ export default function ImageUploader({
 }) {
   const [currentUploads, setCurrentUploads] = useState<Upload[]>([]);
 
+  const uploadImageToBucket = async (index: number) => {
+    const upload = currentUploads[index];
+    if (!upload) {
+      throw new Error("No file");
+    }
+    const res = await fetch(
+      "/api/presigned-url?file=" + upload.file.name + "&bucketKey=" + bucketKey,
+    );
+
+    const { presignedUrl, key } = (await res.json()) as {
+      presignedUrl: string;
+      key: string;
+    };
+    console.log(currentUploads[index]!.status);
+    setCurrentUploads((currentUploads) => {
+      const newCurrentUploads = currentUploads.slice();
+      newCurrentUploads[index]!.status = "uploading";
+      return newCurrentUploads;
+    });
+    console.log(`setting uploading to true for index ${index}`);
+    console.log(currentUploads[index]!.status);
+
+    axios
+      .put(presignedUrl, upload.file, {
+        headers: {
+          "Content-Type": upload.file.type,
+          "Content-Disposition": `attachment; filename="${upload.file.name}"`,
+        },
+        onUploadProgress: (progressEvent) => {
+          if (progressEvent.total) {
+            const progress = Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100,
+            );
+            console.log("checkout progress", progress);
+
+            setCurrentUploads((currentUploads) => {
+              const newCurrentUploads = currentUploads.slice();
+              newCurrentUploads[index]!.progress = progress;
+              return newCurrentUploads;
+            });
+          }
+        },
+      })
+      .then(async (response) => {
+        console.log("response", response);
+        // add to db
+        onUploadComplete({ key: key });
+        // await updateProperty({ coverImageKey: key, propertyId: propertyId });
+        setCurrentUploads((currentUploads) => {
+          const newCurrentUploads = currentUploads.slice();
+          newCurrentUploads[index]!.status = "success";
+          return newCurrentUploads;
+        });
+      })
+      .catch((error) => {
+        console.error("error", error);
+        setCurrentUploads((currentUploads) => {
+          const newCurrentUploads = currentUploads.slice();
+          newCurrentUploads[index]!.status = "error";
+          return newCurrentUploads;
+        });
+      });
+  };
+
   if (currentUploads.length > 0) {
     return (
       <>
-        {currentUploads.map((upload, index) => (
-          <UploadSelectedImage
-            filePreviewUrl={URL.createObjectURL(upload.file)}
-            fileName={upload.file.name}
-            key={index}
-            uploading={upload.uploading}
-            progress={upload.progress}
-          />
-        ))}
+        <div className="flex w-full flex-wrap justify-center gap-8 py-4">
+          {currentUploads.map((upload, index) => (
+            <UploadSelectedImage
+              filePreviewUrl={URL.createObjectURL(upload.file)}
+              fileName={upload.file.name}
+              key={index}
+              status={upload.status}
+              progress={upload.progress}
+              uploadImageToBucket={uploadImageToBucket}
+              index={index}
+            />
+          ))}
+        </div>
         <UploadButton
           currentUploads={currentUploads}
           setCurrentUploads={setCurrentUploads}
-          onUploadComplete={onUploadComplete}
-          bucketKey={bucketKey}
+          uploadImageToBucket={uploadImageToBucket}
         />
       </>
     );
@@ -76,10 +159,18 @@ function SelectImageToUpload({
         console.error("No File");
         return;
       }
+      let status: UploadStatus = "preview";
       // check file type
+      if (!file.type.startsWith("image") && !file.name.endsWith(".pdf")) {
+        status = "fileTypeError";
+      }
+      // check file size
+      if (file.size > 8000000) {
+        status = "fileSizeError";
+      }
       setCurrentUploads((currentUploads) => [
         ...currentUploads,
-        { file, progress: 0, uploading: false },
+        { file, progress: 0, status: status },
       ]);
     }
   };
@@ -110,6 +201,8 @@ function SelectImageToUploadForDesktop({
     error: false,
     errorMessage: "",
   });
+
+  const uploadInputButtonId = useId();
 
   const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -175,10 +268,7 @@ function SelectImageToUploadForDesktop({
         errorMessage={dragError.errorMessage}
       />
       <Text>or</Text>
-      <label
-        htmlFor="upload-cover-image-desktop"
-        className="mb-4 place-self-center"
-      >
+      <label htmlFor={uploadInputButtonId} className="mb-4 place-self-center">
         <div className="bg-brand text-dark hover:bg-brand/70 rounded-full border-0 p-6 text-xl font-extrabold">
           Browse Files
         </div>
@@ -187,8 +277,8 @@ function SelectImageToUploadForDesktop({
           name="file"
           type="file"
           accept="capture=camera,image/*"
-          multiple={false}
-          id="upload-cover-image-desktop"
+          multiple={true}
+          id={uploadInputButtonId}
           hidden
         />
       </label>
@@ -201,13 +291,14 @@ function SelectImageToUploadForMobile({
 }: {
   selectFile: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
+  const uploadMobileInputButtonId = useId();
   return (
     <form
       onSubmit={(e) => e.preventDefault()}
       className="flex h-full flex-col justify-stretch"
     >
       <label
-        htmlFor="upload-cover-image-mobile"
+        htmlFor={uploadMobileInputButtonId}
         className="mb-4 flex h-full flex-col items-center justify-center"
       >
         <div className="bg-brand text-dark hover:bg-brand/70 cursor-pointer rounded-full border-0 p-6 text-xl font-extrabold">
@@ -219,7 +310,7 @@ function SelectImageToUploadForMobile({
           type="file"
           accept="capture=camera,image/*"
           multiple={true}
-          id="upload-cover-image-mobile"
+          id={uploadMobileInputButtonId}
           hidden
         />
       </label>
@@ -230,107 +321,142 @@ function SelectImageToUploadForMobile({
 function UploadSelectedImage({
   filePreviewUrl,
   fileName,
-  uploading,
+  status,
   progress,
+  uploadImageToBucket,
+  index,
 }: {
-  uploading: boolean;
+  status: UploadStatus;
   filePreviewUrl: string | undefined;
   fileName: string;
   progress: number;
+  uploadImageToBucket: (index: number) => void;
+  index: number;
 }) {
-  console.log("progress", progress);
-  console.log("uploading", uploading);
+  // if file is a pdf, show a pdf icon
+  const isPdf = fileName.endsWith(".pdf");
+
   return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <img src={filePreviewUrl} width={120} height={120} />
-      <Text>{fileName}</Text>
-      {uploading && <ProgressBar progress={progress} />}
+    <div className="flex  flex-col items-center justify-center">
+      <div className="w-32 ">
+        {status === "fileTypeError" || status === "fileSizeError" ? (
+          <WarningIcon width={120} height={80} colour="rgb(239 68 68)" />
+        ) : isPdf ? (
+          <PdfFileIcon />
+        ) : (
+          <img src={filePreviewUrl} width={128} height={128} />
+        )}
+        <Text className="text-wrap break-words text-sm">{fileName}</Text>
+        {status === "preview" && (
+          <UploadFromPreview
+            uploadImageToBucket={() => uploadImageToBucket(index)}
+          />
+        )}
+        {status === "uploading" && <ProgressBar progress={progress} />}
+        {status === "error" && (
+          <UploadError uploadImageToBucket={() => uploadImageToBucket(index)} />
+        )}
+        {status === "success" && <UploadSuccess />}
+        {status === "fileTypeError" && <FileError status={status} />}
+        {status === "fileSizeError" && <FileError status={status} />}
+      </div>
     </div>
   );
 }
 
+function FileError({ status }: { status: UploadStatus }) {
+  if (status === "fileSizeError") {
+    return (
+      <Text className="text-sm text-red-500">
+        File size is too large. Max file size is 8MB
+      </Text>
+    );
+  }
+  if (status === "fileTypeError") {
+    return (
+      <Text className="text-sm text-red-500">
+        File type not supported. Only images and pdfs are allowed
+      </Text>
+    );
+  }
+  return <Text className="text-sm text-red-500">Upload Not Allowed</Text>;
+}
+
 function ProgressBar({ progress }: { progress: number }) {
-  return <progress value={progress} max={100} />;
+  return (
+    <div className="w-full">
+      <progress
+        value={progress}
+        max={100}
+        className="[&::-webkit-progress-value]:bg-brand w-full"
+      />
+      <Text className="text-sm">Uploading...</Text>
+    </div>
+  );
+}
+
+function UploadFromPreview({
+  uploadImageToBucket,
+}: {
+  uploadImageToBucket: () => void;
+}) {
+  return (
+    <button
+      onClick={uploadImageToBucket}
+      className="bg-altSecondary flex w-full justify-center rounded-lg p-2 "
+    >
+      <UploadIcon /> <Text className="pl-2 text-sm">Upload</Text>
+    </button>
+  );
+}
+
+function UploadError({
+  uploadImageToBucket,
+}: {
+  uploadImageToBucket: () => void;
+}) {
+  return (
+    <div className="w-full">
+      <div className="flex items-center">
+        <CancelIcon colour="rgb(239 68 68)" width={28} />
+        <Text className="text-sm text-red-500">Upload Failed</Text>
+      </div>
+      <button
+        onClick={uploadImageToBucket}
+        className="bg-altSecondary flex w-full justify-center rounded-lg p-2 "
+      >
+        <RetryIcon width={20} /> <Text className="pl-2">Retry</Text>
+      </button>
+    </div>
+  );
+}
+
+function UploadSuccess() {
+  return (
+    <div className="flex w-full p-2">
+      <ConfirmIcon colour="rgb(21 128 61)" width={28} />
+      <Text className="text-sm text-green-700">Upload Success</Text>
+    </div>
+  );
 }
 
 function UploadButton({
   currentUploads,
   setCurrentUploads,
-  onUploadComplete,
-  bucketKey,
+
+  uploadImageToBucket,
 }: {
   currentUploads: Upload[];
   setCurrentUploads: React.Dispatch<React.SetStateAction<Upload[]>>;
-  onUploadComplete: ({ key }: { key: string }) => void;
-  bucketKey: string;
+
+  uploadImageToBucket: (index: number) => void;
 }) {
-  const uploadImageToBucket = async (index: number) => {
-    const upload = currentUploads[index];
-    if (!upload) {
-      throw new Error("No file");
-    }
-    const res = await fetch(
-      "/api/presigned-url?file=" + upload.file.name + "&bucketKey=" + bucketKey,
-    );
-
-    const { presignedUrl, key } = (await res.json()) as {
-      presignedUrl: string;
-      key: string;
-    };
-    console.log(currentUploads[index]!.uploading);
-    setCurrentUploads((currentUploads) => {
-      const newCurrentUploads = currentUploads.slice();
-      newCurrentUploads[index]!.uploading = true;
-      return newCurrentUploads;
-    });
-    console.log(`setting uploading to true for index ${index}`);
-    console.log(currentUploads[index]!.uploading);
-
-    axios
-      .put(presignedUrl, upload.file, {
-        headers: {
-          "Content-Type": upload.file.type,
-          "Content-Disposition": `attachment; filename="${upload.file.name}"`,
-        },
-        onUploadProgress: (progressEvent) => {
-          if (progressEvent.total) {
-            const progress = Math.round(
-              (progressEvent.loaded / progressEvent.total) * 100,
-            );
-            console.log("checkout progress", progress);
-
-            setCurrentUploads((currentUploads) => {
-              const newCurrentUploads = currentUploads.slice();
-              newCurrentUploads[index]!.progress = progress;
-              return newCurrentUploads;
-            });
-          }
-        },
-      })
-      .then(async (response) => {
-        console.log("response", response);
-        // add to db
-        onUploadComplete({ key: key });
-        // await updateProperty({ coverImageKey: key, propertyId: propertyId });
-        setCurrentUploads((currentUploads) => {
-          const newCurrentUploads = currentUploads.slice();
-          newCurrentUploads[index]!.uploading = true;
-          return newCurrentUploads;
-        });
-      })
-      .catch((error) => {
-        console.error("error", error);
-        setCurrentUploads((currentUploads) => {
-          const newCurrentUploads = currentUploads.slice();
-          newCurrentUploads[index]!.uploading = true;
-          return newCurrentUploads;
-        });
-      });
-  };
-
   const onClickUpload = () => {
     currentUploads.forEach((upload, index) => {
-      uploadImageToBucket(index);
+      // checks status
+      if (upload.status === "preview" || upload.status === "error") {
+        uploadImageToBucket(index);
+      }
     });
   };
 
