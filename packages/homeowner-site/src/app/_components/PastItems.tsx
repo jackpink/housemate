a4cc7e4b39d7b2483f1b4907da523eb6e12c15e0
Jack-Desktop <jackpinkerton@live.com.au>
2024-05-27 20:38:12 +1000

    Working on filters

--- packages/homeowner-site/src/app/_components/PastItems.tsx
+++ packages/homeowner-site/src/app/_components/PastItems.tsx
@@ -3,6 +3,16 @@
 import React from "react";
 import { CompletedItems } from "../../../../core/homeowner/item";
 import { ItemQuickViewDialog } from "./ToDos";
+import clsx from "clsx";
+import { PlusIcon } from "../../../../ui/Atoms/Icons";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeading,
+  DialogTrigger,
+} from "../../../../ui/Atoms/Dialog";
 
 export default function PastItems({
   completedItems,
@@ -36,10 +46,54 @@ FOR MOBILE
 ##########################################################################
 */
 
+export const AddButton = ({
+  onClick,
+  editable = true,
+}: {
+  onClick: () => void;
+  editable?: boolean;
+}) => {
+  let colour = "#c470e7";
+  if (!editable) {
+    colour = "#c4c4c4";
+  }
+  return (
+    <button
+      className={clsx(
+        "p-6 text-xl text-brandSecondary",
+        !editable && "cursor-not-allowed text-slate-500",
+      )}
+      onClick={onClick}
+    >
+      <div className="flex items-center justify-center">
+        <PlusIcon width={25} height={25} colour={colour} />
+        <span className="pl-4">Add Filter</span>
+      </div>
+    </button>
+  );
+};
+
 function FiltersForMobile() {
   return (
     <div>
-      <h1>Filters for Mobile</h1>
+      <Dialog>
+        <DialogTrigger asChild>
+          <AddButton onClick={() => {}} />
+        </DialogTrigger>
+        <DialogContent className="Dialog">
+          <DialogClose className="float-end rounded-lg border-2 border-black p-2">
+            <p>Close</p>
+          </DialogClose>
+          <DialogHeading className="pt-3 text-xl">Filters</DialogHeading>
+          <DialogDescription className="flex w-full flex-col items-center pt-4">
+            <div>
+              <input type="checkbox" />
+              <label>Title</label>
+              <input type="text" placeholder="Enter Title to Search" />
+            </div>
+          </DialogDescription>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
