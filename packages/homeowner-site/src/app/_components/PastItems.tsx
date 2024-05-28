fb0c87c3a7dca6cabc8e654ef1ae09a9b908ee0d
Jack-Desktop <jackpinkerton@live.com.au>
2024-05-28 20:31:43 +1000

    add filters dialog created

--- packages/homeowner-site/src/app/_components/PastItems.tsx
+++ packages/homeowner-site/src/app/_components/PastItems.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { use, useCallback } from "react";
 import { CompletedItems } from "../../../../core/homeowner/item";
 import { ItemQuickViewDialog } from "./ToDos";
 import clsx from "clsx";
@@ -13,6 +13,7 @@ import {
   DialogHeading,
   DialogTrigger,
 } from "../../../../ui/Atoms/Dialog";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function PastItems({
   completedItems,
@@ -73,28 +74,123 @@ export const AddButton = ({
   );
 };
 
+type filterValues = "title";
+
+type Filter = { status: boolean; value: string };
+
+type Filters = { title: Filter };
+
 function FiltersForMobile() {
+  const [filters, setFilters] = React.useState<{ title: Filter }>({
+    title: { status: false, value: "" },
+  });
+
   return (
     <div>
-      <Dialog>
-        <DialogTrigger asChild>
-          <AddButton onClick={() => {}} />
-        </DialogTrigger>
-        <DialogContent className="Dialog">
-          <DialogClose className="float-end rounded-lg border-2 border-black p-2">
-            <p>Close</p>
-          </DialogClose>
-          <DialogHeading className="pt-3 text-xl">Filters</DialogHeading>
-          <DialogDescription className="flex w-full flex-col items-center pt-4">
-            <div>
-              <input type="checkbox" />
-              <label>Title</label>
-              <input type="text" placeholder="Enter Title to Search" />
-            </div>
-          </DialogDescription>
-        </DialogContent>
-      </Dialog>
+      <AddFilterDialog filters={filters} setFilters={setFilters} />
     </div>
+  );
+}
+
+function AddFilterDialog({
+  filters,
+  setFilters,
+}: {
+  filters: Filters;
+  setFilters: React.Dispatch<React.SetStateAction<Filters>>;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const removeQueryString = useCallback(
+    (name: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.delete(name);
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const toggleFilterStatus = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name } = e.target;
+      const newStatus = !filters[name as filterValues].status;
+      const currentValue = filters[name as filterValues].value;
+      setFilters({
+        ...filters,
+        [name as filterValues]: {
+          status: newStatus,
+          value: currentValue,
+        },
+      });
+      if (!!newStatus) {
+        const newQueryString = createQueryString(name, currentValue);
+        router.push(`${pathname}?${newQueryString}`);
+      } else {
+        const newQueryString = removeQueryString(name);
+        router.push(`${pathname}?${newQueryString}`);
+      }
+    },
+    [filters],
+  );
+
+  const handleFilterChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value, name } = e.target;
+      setFilters({
+        ...filters,
+        [name as filterValues]: {
+          status: filters[name as filterValues].status,
+          value,
+        },
+      });
+      if (filters[name as filterValues]) {
+        const newQueryString = createQueryString(name, value);
+        router.push(`${pathname}?${newQueryString}`);
+      }
+    },
+    [filters],
+  );
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <AddButton onClick={() => {}} />
+      </DialogTrigger>
+      <DialogContent className="Dialog">
+        <DialogClose className="float-end rounded-lg border-2 border-black p-2">
+          <p>Close</p>
+        </DialogClose>
+        <DialogHeading className="pt-3 text-xl">Filters</DialogHeading>
+        <DialogDescription className="flex w-full flex-col items-center pt-4">
+          <div>
+            <input
+              type="checkbox"
+              checked={filters.title.status}
+              name="title"
+              onChange={toggleFilterStatus}
+            />
+            <label>Title</label>
+            <input
+              type="text"
+              placeholder="Enter Title to Search"
+              onChange={handleFilterChange}
+              name="title"
+            />
+          </div>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 }
 
