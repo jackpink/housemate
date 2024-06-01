2ccbe3325863a968d5809b29a912cf59c33d2765
Jack-Desktop <jackpinkerton@live.com.au>
2024-06-01 22:13:58 +1000

    Mange account

--- packages/homeowner-site/src/app/(main app)/manage-account/page.tsx
+++ packages/homeowner-site/src/app/(main app)/manage-account/page.tsx
@@ -3,6 +3,7 @@ import { PageWithSingleColumn } from "../../../../../ui/Atoms/PageLayout";
 import { auth } from "~/auth";
 import { redirect } from "next/navigation";
 import { User } from "../../../../../core/homeowner/user";
+import { CTAButton } from "../../../../../ui/Atoms/Button";
 
 export default async function ManageAccountPage() {
   const session = await auth();
@@ -24,10 +25,14 @@ export default async function ManageAccountPage() {
       <PageTitle>Manage Account</PageTitle>
 
       <PageWithSingleColumn>
-        <h1>General</h1>
-        <p>Email: {user.email}</p>
-        <p>First Name: {user.firstName}</p>
-        <p>Last Name: {user.lastName}</p>
+        <h1 className="border-b-2 border-black font-semibold">General</h1>
+        <div className="flex flex-col items-center justify-center">
+          <p>Email: {user.email}</p>
+          <p>First Name: {user.firstName}</p>
+          <p>Last Name: {user.lastName}</p>
+          <CTAButton rounded>Change Password</CTAButton>
+        </div>
+        <h1 className="border-b-2 border-black font-semibold">Alerts</h1>
       </PageWithSingleColumn>
     </>
   );
---
+++ packages/homeowner-site/src/app/_components/ManageAccount.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { User } from "../../../../core/homeowner/user";
+
+export function GeneralSettings({ user }: { user: User }) {
+  return (
+    <div>
+      <h1>Manage Account</h1>
+    </div>
+  );
+}
+
+export function AlertSettings() {
+  return (
+    <div>
+      <h1>Alerts</h1>
+    </div>
+  );
+}
