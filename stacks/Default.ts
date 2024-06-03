39bff582995921d33cbfb4feea0dd5ed558b2812
Jack-Desktop <jackpinkerton@live.com.au>
2024-06-03 21:02:00 +1000

    still adding cron job

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -1,8 +1,26 @@
-import { StackContext, NextjsSite, Bucket } from "sst/constructs";
+import { StackContext, NextjsSite, Bucket, Cron } from "sst/constructs";
 import { env } from "../packages/core/env.mjs";
 
 export function Default({ stack }: StackContext) {
   const itemUploadsBucket = new Bucket(stack, "ItemUploads");
+
+  new Cron(stack, "Alerts Cron", {
+    schedule: "rate(1 day)",
+    job: {
+      environment: {
+        TURSO_CONNECTION_URL: env.TURSO_CONNECTION_URL,
+        TURSO_AUTH_TOKEN: env.TURSO_AUTH_TOKEN,
+        COGNITO_CLIENT_ID: env.COGNITO_CLIENT_ID,
+        COGNITO_CLIENT_SECRET: env.COGNITO_CLIENT_SECRET,
+        COGNITO_ISSUER: env.COGNITO_ISSUER,
+        AUTH_SECRET: env.AUTH_SECRET,
+        AUTH_URL: env.AUTH_URL,
+        AUTH_TRUST_HOST: env.AUTH_TRUST_HOST,
+        GOOGLE_MAPS_API_KEY: env.GOOGLE_MAPS_API_KEY,
+      },
+      handler: "alerts",
+    },
+  });
 
   const site = new NextjsSite(stack, "site", {
     bind: [itemUploadsBucket],
