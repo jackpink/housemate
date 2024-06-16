5dad7b7620b455b45fde87da30ecc5959adae5dd
Jack-Desktop <jackpinkerton@live.com.au>
2024-06-17 09:20:56 +1000

    get schdule works for items and past items, need warranty now as well

--- packages/core/homeowner/item.ts
+++ packages/core/homeowner/item.ts
@@ -456,11 +456,13 @@ async function getItemPastDatesInDateRange({
         filesRootFolder: {
           with: { files: true },
         },
+        pastDates: true,
       },
     });
 
     if (item) {
       item.date = itemPastDateObj.date;
+      item.status = ItemStatus.COMPLETED;
       items.push(item);
     }
   }
@@ -479,20 +481,45 @@ export async function getSchedule({
   futureMonths: number;
 }) {
   const items = await db.query.item.findMany({
-    where: (item, { eq, and }) => eq(item.propertyId, propertyId),
+    where: eq(item.propertyId, propertyId),
+    with: {
+      filesRootFolder: {
+        with: { files: true },
+      },
+      pastDates: true,
+    },
   });
+  const endDate = new Date(currentDate);
   //curretnDate plus 6 months
-  const endDate = new Date(
-    currentDate.setMonth(currentDate.getMonth() + futureMonths),
-  );
+  endDate.setMonth(currentDate.getMonth() + futureMonths);
+
   const startDate = new Date(
     currentDate.setMonth(currentDate.getMonth() - pastMonths),
   );
+  console.log("start and end date", startDate, endDate);
 
   const filteredItems = items.filter((item) => {
     const itemDate = new Date(item.date);
     return itemDate >= startDate && itemDate <= endDate;
   });
 
-  return filteredItems;
+  const itemPastDates = await getItemPastDatesInDateRange({
+    propertyId,
+    startDate: startDate.toISOString(),
+    endDate: endDate.toISOString(),
+  });
+
+  const allItems = [...filteredItems, ...itemPastDates];
+
+  allItems.sort(dateOrdering);
+
+  return allItems;
+}
+
+export type ScheduledItems = Awaited<ReturnType<typeof getSchedule>>;
+
+function dateOrdering(a: ScheduledItems[number], b: ScheduledItems[number]) {
+  const dateA = new Date(a.date);
+  const dateB = new Date(b.date);
+  return dateA.getTime() - dateB.getTime();
 }
