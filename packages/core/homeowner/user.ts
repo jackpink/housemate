30754a4803dd81027a94f3cd2cf7a105d8a72930
Jack-Desktop <jackpinkerton@live.com.au>
2024-07-21 21:45:34 +1000

    user email code verfication, needs testin

--- packages/core/homeowner/user.ts
+++ packages/core/homeowner/user.ts
@@ -123,7 +123,35 @@ export async function verifyEmailVerificationCode({
   userId: string;
   code: string;
 }) {
-  console.log("Try to verify email verification code", userId, code);
+  console.log("Try to verify email verification code", userId);
+  const result = await db.transaction(async (tx) => {
+    const [userCode] = await tx
+      .select()
+      .from(schema.emailVerificationCode)
+      .where(and(eq(schema.emailVerificationCode.userId, userId)));
+    if (!userCode || userCode.code !== code) {
+      tx.rollback();
+      return false;
+    }
+    // isWithinExpirationDate
+    if (userCode.expiresAt < new Date()) {
+      // code has expired
+      tx.rollback();
+      return false;
+    }
+    // delete and return true
+    await tx
+      .delete(schema.emailVerificationCode)
+      .where(eq(schema.emailVerificationCode.id, userCode.id));
+
+    await tx
+      .update(schema.homeownerUsers)
+      .set({ emailVerified: true })
+      .where(eq(schema.homeownerUsers.id, userId));
+
+    return true;
+  });
+  return result;
 }
 
 export async function hasActiveVerificationCode({
