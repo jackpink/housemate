33c8c417f1711a853d944ba745eedbbd3e4f2cc2
Jack-Desktop <jackpinkerton@live.com.au>
2024-08-21 20:29:32 +1000

    top banner of landing page done

--- packages/homeowner-site/src/app/page.tsx
+++ packages/homeowner-site/src/app/page.tsx
@@ -1,30 +1,44 @@
 import Link from "next/link";
-import Logo from "../../../ui/Atoms/Logo";
+import Logo, { HorizontalLogo } from "../../../ui/Atoms/Logo";
 import { Text } from "../../../ui/Atoms/Text";
 import {
   LargeButton,
   LargeButtonContent,
   LargeButtonTitle,
 } from "../../../ui/Atoms/Button";
+import Image from "next/image";
+import computerMarketing from "../../public/laptopmarketingimage.png";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <Logo width="300px" height="300px" fillColour="#c470e7" />
-          <div className="flex flex-col items-center justify-center gap-4 text-center ">
-            <Text>Welcome to Prop Doc the property maintenance app. </Text>
-            <Text>Management all of the work done on your home.</Text>
-            <Text>Search photos and documents for past work doe in rooms.</Text>
-            <Link href="/properties" className="">
-              <LargeButton>
-                <LargeButtonTitle>Homepage</LargeButtonTitle>
-                <LargeButtonContent>
-                  Explore your properties and latest jobs
-                </LargeButtonContent>
-              </LargeButton>
-            </Link>
+    <main className=" min-h-screen ">
+      <div className="flex w-full items-center justify-center bg-brand">
+        <div className="grid max-w-screen-lg gap-8 md:grid-cols-2 ">
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <HorizontalLogo height={50} fillColour="#c470e7" />
+            <div className="flex flex-col items-center justify-center gap-4 text-center ">
+              <p className="text-2xl font-bold">
+                Manage all your home management tasks.
+              </p>
+              <p className="text-lg font-semibold">
+                Your home is your most important asset. It's crucial to stay on
+                top of tasks and keep detailed records of all work done.
+              </p>
+              <Link
+                href="/sign-up"
+                className="rounded-full bg-brandSecondary p-4 px-10 text-lg font-bold"
+              >
+                Get Started
+              </Link>
+            </div>
+          </div>
+          <div className="hidden items-center justify-center md:flex">
+            <Image
+              src={computerMarketing}
+              alt="Computer Marketing"
+              width={500}
+              height={500}
+            />
           </div>
         </div>
       </div>
