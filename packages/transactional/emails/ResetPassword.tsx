5a24dfb234f13d8e3db5ff803950ec30c23c1b1f
Jack-Desktop <jackpinkerton@live.com.au>
2024-08-21 20:30:46 +1000

    reset password looking decent

--- packages/transactional/emails/ResetPassword.tsx
+++ packages/transactional/emails/ResetPassword.tsx
@@ -25,24 +25,25 @@ export default function PasswordResetEmail({ code }: { code: string }) {
       <Tailwind>
         <Body className="mx-auto my-auto bg-white px-2 font-sans">
           <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
-            <Section className="mt-[32px]">
+            <Section className="mx-auto mt-[32px]">
               <Img
                 src="static/logo.png"
                 width="300"
                 height="300"
                 alt="housemate"
+                className="mx-auto"
               />
             </Section>
             <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
               <strong>Reset your password</strong>
             </Heading>
-            <Text className="text-[14px] leading-[24px] text-black">
+            <Text className="text-center text-[14px] leading-[24px] text-black">
               Click the following link to set a new password.
             </Text>
 
             <Section className="mb-[32px] mt-[32px] text-center">
               <Link
-                className="rounded bg-[#000000] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+                className="rounded bg-[#7df2cd] px-5 py-3 text-center text-[12px] font-semibold text-black no-underline"
                 href={"https://housemate.dev/password-reset/" + code}
               >
                 Reset Password Link
