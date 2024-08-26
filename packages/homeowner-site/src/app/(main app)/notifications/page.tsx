8461074cf8c3f7892e0e880ee14af848b26a6d82
Jack-Desktop <jackpinkerton@live.com.au>
2024-08-26 16:43:57 +1000

    chnaging notifcations page

--- packages/homeowner-site/src/app/(main app)/notifications/page.tsx
+++ packages/homeowner-site/src/app/(main app)/notifications/page.tsx
@@ -4,6 +4,13 @@ import { redirect } from "next/navigation";
 import { Alert } from "../../../../../core/homeowner/alert";
 import { Alerts } from "~/app/_components/Alerts";
 import { getVerifiedUserOrRedirect } from "~/utils/pageRedirects";
+import Nav from "~/app/_components/Nav";
+import { Property } from "../../../../../core/homeowner/property";
+
+async function getProperties({ userId }: { userId: string }) {
+  console.log("Getting properties for user", userId);
+  return Property.getByHomeownerId(userId);
+}
 
 export default async function AlertsPage() {
   const user = await getVerifiedUserOrRedirect();
@@ -12,16 +19,20 @@ export default async function AlertsPage() {
     // redirect to login
     redirect("/sign-in");
   }
-
+  const properties = await getProperties({ userId: user.id });
   const alerts = await Alert.getForHomeowner(user.id);
 
   return (
     <>
+      <Nav properties={properties} currentPropertyId={""} />
       <PageTitle>Alerts</PageTitle>
 
-      <PageWithSingleColumn>
-        <Alerts alerts={alerts} />
-      </PageWithSingleColumn>
+      <div className="mx-auto w-full md:w-112 ">
+        <h1 className="border-b-2 border-black font-semibold">General</h1>
+        <div className="flex flex-col items-center justify-center">
+          <Alerts alerts={alerts} />
+        </div>
+      </div>
     </>
   );
 }
