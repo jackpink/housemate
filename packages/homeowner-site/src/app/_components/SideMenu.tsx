376bd8847f61ae9e19d7f72f16338c39f92e9151
Jack-Desktop <jackpinkerton@live.com.au>
2024-08-28 19:45:35 +1000

    side menu click response

--- packages/homeowner-site/src/app/_components/SideMenu.tsx
+++ packages/homeowner-site/src/app/_components/SideMenu.tsx
@@ -20,51 +20,64 @@ export default function SideMenu({
 }) {
   return (
     <aside className="hidden max-w-min xs:block">
-      <Link
+      <SideMenuLinkButton
         href={`/properties/${propertyId}/todo`}
-        className={clsx("block", selected === "todo" && "bg-altSecondary")}
-      >
-        <div className="h-full w-full  p-7">
-          <div className="flex w-full flex-col items-center justify-center">
-            <ToDoListIcon width={45} height={45} />
-            <Text className="text-xl font-bold">To Do List</Text>
-          </div>
-        </div>
-      </Link>
-      <Link
-        href={`/properties/${propertyId}/schedule`}
-        className={clsx("block", selected === "schedule" && "bg-altSecondary")}
-      >
-        <div className="h-full w-full p-7">
-          <div className="flex w-full flex-col items-center justify-center">
-            <ScheduleIcon width={45} height={45} />
-            <Text className="text-xl font-bold">Schedule</Text>
-          </div>
-        </div>
-      </Link>
+        selected={selected === "todo"}
+        Icon={<ToDoListIcon width={45} height={45} />}
+        title="To Do List"
+      />
 
-      <Link
+      <SideMenuLinkButton
+        href={`/properties/${propertyId}/schedule`}
+        selected={selected === "schedule"}
+        Icon={<ScheduleIcon width={45} height={45} />}
+        title="Schedule"
+      />
+
+      <SideMenuLinkButton
         href={`/properties/${propertyId}/search`}
-        className={clsx("block", selected === "search" && "bg-altSecondary")}
-      >
-        <div className="h-full w-full  p-7">
-          <div className="flex w-full flex-col items-center justify-center">
-            <LargeSearchIcon width={45} height={45} />
-            <Text className="text-xl font-bold">Search</Text>
-          </div>
-        </div>
-      </Link>
-      <Link
+        selected={selected === "search"}
+        Icon={<LargeSearchIcon width={45} height={45} />}
+        title="Search"
+      />
+
+      <SideMenuLinkButton
         href={`/properties/${propertyId}/add`}
-        className={clsx("block", selected === "add" && "bg-altSecondary")}
-      >
-        <div className="h-full w-full  p-7">
-          <div className="flex w-full flex-col items-center justify-center">
-            <LargeAddIcon width={45} height={45} />
-            <Text className="text-xl font-bold">Add Task</Text>
-          </div>
-        </div>
-      </Link>
+        selected={selected === "add"}
+        Icon={<LargeAddIcon width={45} height={45} />}
+        title="Add Task"
+      />
     </aside>
+  );
+}
+
+function SideMenuLinkButton({
+  href,
+  selected,
+  Icon,
+  title,
+}: {
+  href: string;
+  selected: boolean;
+  Icon: React.ReactNode;
+  title: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className={clsx(
+        "group block ",
+        selected
+          ? "bg-altSecondary"
+          : "focus:animate-pulse focus:bg-altSecondary",
+      )}
+    >
+      <div className="h-full w-full  p-7">
+        <div className="flex w-full flex-col items-center justify-center">
+          {Icon}
+          <Text className="text-xl font-bold">{title}</Text>
+        </div>
+      </div>
+    </Link>
   );
 }
