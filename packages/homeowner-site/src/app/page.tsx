709ee181a4853f4cc9bafcaf89f9d5828c87c7bc
Jack-Desktop <jackpinkerton@live.com.au>
2024-09-22 08:53:54 +1000

    changed layout and background on marketing page

--- packages/homeowner-site/src/app/page.tsx
+++ packages/homeowner-site/src/app/page.tsx
@@ -1,15 +1,7 @@
 import Link from "next/link";
 import Logo, { HorizontalLogo } from "../../../ui/Atoms/Logo";
-import { Text } from "../../../ui/Atoms/Text";
-import {
-  LargeButton,
-  LargeButtonContent,
-  LargeButtonTitle,
-} from "../../../ui/Atoms/Button";
 import Image from "next/image";
 import computerMarketing from "../../public/laptopmarketingimage.png";
-import scheduleMarketing from "../../public/schedulemarketingimage.png";
-import todoMarketing from "../../public/todomarketingimage.png";
 import remindersMarketing from "../../public/remindersmarketingimage.png";
 import { EmailSentIcon, ScheduleIcon, ToDoIcon } from "../../../ui/Atoms/Icons";
 import scheduleGif from "../../public/schedule-show.gif";
@@ -18,41 +10,31 @@ import todoGif from "../../public/todo-complete.gif";
 export default function HomePage() {
   return (
     <main className=" min-h-screen ">
-      <div className="flex w-full items-center justify-center bg-brand">
-        <div className="grid max-w-screen-lg gap-8 md:grid-cols-2 ">
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            <HorizontalLogo height={50} fillColour="#c470e7" />
-            <div className="flex flex-col items-center justify-center gap-4 text-center ">
-              <div className="items-center justify-center md:hidden">
-                <Image
-                  src={computerMarketing}
-                  alt="Schedule Marketing"
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <p className="text-2xl font-bold">
-                Manage all your home management tasks.
-              </p>
-              <p className="text-lg font-semibold">
-                Your home is your most important asset. It's crucial to stay on
-                top of tasks and keep detailed records of all work done.
-              </p>
-              <Link
-                href="/sign-up"
-                className="rounded-full bg-brandSecondary p-4 px-10 text-lg font-bold"
-              >
-                Get Started
-              </Link>
+      <div className="flex w-full items-center justify-center ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <Logo height="300" width="300" fillColour="#7df2cd" />
+          <div className="flex flex-col items-center justify-center gap-4 text-center ">
+            <div className="items-center justify-center md:hidden">
+              <Image
+                src={computerMarketing}
+                alt="Schedule Marketing"
+                width={200}
+                height={200}
+              />
             </div>
-          </div>
-          <div className="hidden items-center justify-center md:flex">
-            <Image
-              src={computerMarketing}
-              alt="Computer Marketing"
-              width={500}
-              height={500}
-            />
+            <p className="text-3xl font-bold">
+              Manage all your home management tasks.
+            </p>
+            <p className="text-lg font-semibold">
+              Your home is your most important asset. It's crucial to stay on
+              top of tasks and keep detailed records of all work done.
+            </p>
+            <Link
+              href="/sign-up"
+              className="rounded-full bg-brandSecondary p-4 px-10 text-lg font-bold"
+            >
+              Get Started
+            </Link>
           </div>
         </div>
       </div>
